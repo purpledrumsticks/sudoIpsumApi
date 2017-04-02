e3068cebffa7ce247017b74336e18e2598ce589e
Corey Slaven <coreyslaven@aol.com>
2017-04-01 23:19:35 -0400

    More changes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.use(allowCrossDomain)
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.get('/:sudoIpsum', (req, res) => {
    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
